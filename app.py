73c01a5bef88362b142aa10901d04f9c40490a1c
oscarismael47 <oscar.ismael.frausto.perez@gmail.com>
2025-08-16 20:50:25 -0600

    UPDATED: message

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 if "chat_history_summary" not in st.session_state:
-    st.session_state.chat_history_summary = "None"
+    st.session_state.chat_history_summary = ""
 
 
 st.title("Chatbot")
